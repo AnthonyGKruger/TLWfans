494fe64c8877f13206e907b9b19e63ea0483405d
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2023-06-06 05:11:36 +0200

    added favicon and title tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>TLW fans</title>
+        <link rel="icon" href="/assets/logos/LogoTill-72dpi.jpg"/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
