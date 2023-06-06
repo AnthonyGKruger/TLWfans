8149e02e5b951974f2511b274fbde17c4fea3b4d
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2023-06-06 05:14:09 +0200

    added favicon and title tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,16 @@
 import "@/styles/globals.css";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
 				<title>TLW fans</title>
-        <link rel="icon" href="/assets/logos/LogoTill-72dpi.jpg"/>
+				<link rel="icon" href="/assets/logos/LogoTill-72dpi.jpg" />
 			</Head>
 			<Component {...pageProps} />
+			<Analytics />
 		</>
 	);
 }
